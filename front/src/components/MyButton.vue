2edb8ad3acc8fcd5248ef9eac4acffb718e9e3b6
RonRole <ronrolenewadress@gmail.com>
2020-12-24 14:34:43 +0900

    コンポーネント作成テスト

---
+++ front/src/components/MyButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button @click='sawai("ぷももえんぐえげぎぎおんもえちょっちょっちゃっさ！")'>{{greet}}</button>
+</template>
+
+<script lang="ts">
+    import {defineComponent} from 'vue'
+    export default defineComponent({
+        props: {
+            greet: String,
+        },
+        methods: {
+            sawai: (input:string) : void => {
+                alert(input)
+            }
+        }
+    })
+</script>
